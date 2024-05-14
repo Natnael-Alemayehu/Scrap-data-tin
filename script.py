5735924c3fc7070111f62b90ffcaea76548a0d44
Natnael Alemayehu <se.natnael.alemayehu23@gmail.com>
2024-05-14 11:05:42 +0300

    Business name and registered number is getting printed

--- script.py
+++ script.py
@@ -11,16 +11,16 @@ with open('content.txt', 'w') as file:
 
 
 # Find the specific div tag
-div_tag = soup.find('div', {'class': 'flex flex-col items-start'})
-
-# Check if the div tag was found
-if div_tag:
-    # Extract the text from the span tag inside the div
-    span_tag = div_tag.find('span')
-    if span_tag:
-        business_name = span_tag.text.strip()
-        print(business_name)
+div_tags = soup.find_all('div', {'class': 'flex flex-col items-start'})
+# print(type(div_tags))
+for i,div_tag in enumerate(div_tags):
+    if div_tag:
+        span_tag = div_tag.find('span')
+        if i == 0:
+            registered_number = span_tag.text
+            print(f"Registered number: {registered_number}")
+        if i == 1:
+            business_name = span_tag.text
+            print(f"Business name: {business_name}")
     else:
-        print("Could not find the span tag inside the div.")
-else:
-    print("Could not find the div tag with the specified class name.")
+        print("can't find span tag")
